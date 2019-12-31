d97deb312664291a5724c48a685f4f7573e42c31
Will Boyce <me@willboyce.com>
2019-12-31 12:13:18 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name="ha-fsm",
+    version="dev",
+    url="https://github.com/petertufvesson/ha-fsm",
+    py_modules=["fsm"],
+)
